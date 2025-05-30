dc7170ce9da628488fd4263730b151ae97ee4671
RN662 <coppe62@yahoo.co.jp>
2025-05-30 22:32:12 +0900

    初回コミット

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+const date = today.getDate();
+
+const month = today.getMonth() + 1;
+
+const year = today.getFullYear();
+
+console.log((year + '年') + (month + '月') + (date + '日'));
